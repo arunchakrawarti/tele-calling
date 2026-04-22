4398f43b8e2aa4439994aa8ae27911e9f404d701
arunchakrawarti <arunmojampur@gmail.com>
2026-04-22 18:47:15 +0530

    feat:chnage the grid

--- src/components/pages/reports/SummaryStats.jsx
+++ src/components/pages/reports/SummaryStats.jsx
@@ -36,7 +36,7 @@ const SummaryStats = () => {
     }
   ];
   return (
-   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
       {summaryData.map((data, index) => (
         <StatsCard 
           key={index}
